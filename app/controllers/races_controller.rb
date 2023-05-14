19db16e2f1d4c5a8b7f4df2dfc37f1b7f1e3c6b2
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-05-14 17:12:37 -0600

    Feat: Add Edit Race Button to Race show page

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -25,4 +25,22 @@ class RacesController < ApplicationController
     race.save
     redirect_to '/races'
   end
+
+  def edit
+    @race = Race.find(params[:id])
+  end
+
+  def update
+    race = Race.find(params[:id])
+    race.update({
+      location:  params[:race][:location],
+      terrain:   params[:race][:terrain],
+      weather:   params[:race][:weather],
+      length:    params[:race][:length],
+      loop:      params[:race][:loop],
+      overnight: params[:race][:overnight]
+    })
+    race.save
+    redirect_to "/races/#{race.id}"
+  end
 end
